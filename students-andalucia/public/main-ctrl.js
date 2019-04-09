6b5828934f8c020a63bef1e4ada8d7e217a60a01
sos1819mvm <marta_v_m6@hotmail.com>
2019-04-09 12:37:12 +0000

    modular

---
+++ students-andalucia/public/main-ctrl.js
@@ -0,0 +1,12 @@
+var app = angular.module("MiniPostmanApp");
+
+app.controller("MainCtrl", ["$scope","$http", function($scope, $http){
+    console.log("Modular MainCtrl initialized!");
+    $scope.url = "api/v1/students-andalucia";
+                
+    $scope.send = function(){
+        $http.get($scope.url).then(function(response){
+            $scope.data = JSON.stringify(response.data,null,2); 
+        });
+    }
+}]);
