796762707dc751e39e041e96f29ef3a5e3a07166
sos1819egv <enrique4597@gmail.com>
2019-04-10 10:02:39 +0000

    .

--- libraries-stats/index.js
+++ libraries-stats/index.js
@@ -81,86 +81,89 @@ module.exports = function(app, API_PATH, librariestats) {
         });
     });
 
-    // GET Recurso Completo BUSQUEDA Y PAGINACION
-    app.get(API_PATH + "/libraries-stats", function(req, res) {
-        var dbquery = {};
-        let offset = 0;
-        let limit = Number.MAX_SAFE_INTEGER;
+    //GET /studentsAndalucia/
 
-        if (req.query.offset) {
-            offset = parseInt(req.query.offset);
-            delete req.query.offset;
-        }
-        if (req.query.limit) {
-            limit = parseInt(req.query.limit);
-            delete req.query.limit;
-        }
 
-        Object.keys(req.query).forEach((i) => {
-            if (isNaN(req.query[i]) == false) {
-                dbquery[i] = parseInt(req.query[i]);
+    app.get(API_PATH + "/students-andalucia", function(req, res) {
+            var dbquery = {};
+            let offset = 0;
+            let limit = Number.MAX_SAFE_INTEGER;
+    
+            if (req.query.offset) {
+                offset = parseInt(req.query.offset);
+                delete req.query.offset;
             }
-            else {
-                dbquery[i] = req.query[i];
+            if (req.query.limit) {
+                limit = parseInt(req.query.limit);
+                delete req.query.limit;
             }
+    
+            Object.keys(req.query).forEach((i) => {
+                if (isNaN(req.query[i]) == false) {
+                    dbquery[i] = parseInt(req.query[i]);
+                }
+                else {
+                    dbquery[i] = req.query[i];
+                }
+            });
+    
+            if (Object.keys(req.query).includes('from') && Object.keys(req.query).includes('to')) {
+                delete dbquery.from;
+                delete dbquery.to;
+                dbquery['city'] = { "$lte": parseInt(req.query['to']), "$gte": parseInt(req.query['from']) };
+            }
+            else if (Object.keys(req.query).includes('from')) {
+                delete dbquery.from;
+                dbquery['city'] = { "$gte": parseInt(req.query['from']) };
+            }
+            else if (Object.keys(req.query).includes('to')) {
+                delete dbquery.to;
+                dbquery['city'] = { "$lte": parseInt(req.query['to']) };
+            }
+    
+            studentsAndalucia.find(dbquery).skip(offset).limit(limit).toArray((err, studentsArray) => {
+                if (err) {
+                    console.error(" Error accesing DB");
+                    res.sendStatus(500);
+                    return;
+                }
+                else if (studentsArray.length == 0) {
+                    res.send([]);
+                }
+                else {
+                    res.send(studentsArray.map((s) => {
+                        delete s._id;
+                        return s;
+                    }));
+                }
+            });
         });
-
-        if (Object.keys(req.query).includes('from') && Object.keys(req.query).includes('to')) {
-            delete dbquery.from;
-            delete dbquery.to;
-            dbquery['city'] = { "$lte": parseInt(req.query['to']), "$gte": parseInt(req.query['from']) };
-        }
-        else if (Object.keys(req.query).includes('from')) {
-            delete dbquery.from;
-            dbquery['city'] = { "$gte": parseInt(req.query['from']) };
-        }
-        else if (Object.keys(req.query).includes('to')) {
-            delete dbquery.to;
-            dbquery['city'] = { "$lte": parseInt(req.query['to']) };
-        }
-
-        librariestats.find(dbquery).skip(offset).limit(limit).toArray((err, librariesFilter) => {
-            if (err) {
-                console.error(" Error accesing DB");
-                res.sendStatus(500);
-                return;
-            }
-            else if (librariesFilter.length == 0) {
-                res.send([]);
-            }
-            else {
-                res.send(librariesFilter.map((s) => {
-                    delete s._id;
-                    return s;
-                }));
-            }
-        });
-    });
-
-    // POST Recurso Completo
-    app.post(API_PATH + "/libraries-stats/docs", (req, res) => {
-        var newLibrariesStats = req.body;
+    
+    //POST /studentsAndalucia/
+    
+    app.post(API_PATH + "/students-andalucia", (req, res) => {
+        var newStudentsAndalucia = req.body;
         var city = req.body.city;
     
-        librariestats.find({ "city": city }).toArray((err, librariesArray) => {
+        studentsAndalucia.find({ "city": city }).toArray((err, studentsArray) => {
             if (err)
                 console.log(err);
     
-            if (librariesArray != 0) {
+            if (studentsArray != 0) {
                 console.log("Conflicto porque el objeto ya existe");
                 res.sendStatus(409);
     
             }
-            else if (!newLibrariesStats.city || !newLibrariesStats.year ||
-                !newLibrariesStats.number || !newLibrariesStats.activities ||
-                !newLibrariesStats.service || Object.keys(newLibrariesStats).length != 5) {
+            else if (!newStudentsAndalucia.city || !newStudentsAndalucia.year ||
+                !newStudentsAndalucia.eso || !newStudentsAndalucia.high ||
+                !newStudentsAndalucia.vocational || Object.keys(newStudentsAndalucia).length != 5) {
     
                 console.log("El número de campos debe ser 5");
                 res.sendStatus(400);
             }
             else {
                 console.log("El nuevo dato se ha insertado correctamente");
-                librariestats.insert(newLibrariesStats);
+                studentsAndalucia.insert(newStudentsAndalucia);
     
                 res.sendStatus(201);
             }
@@ -168,125 +171,148 @@ module.exports = function(app, API_PATH, librariestats) {
         });
     
     });
-
-    //PUT INCORRECTO
-    app.put(API_PATH + "/libraries-stats", (req, res) => {
-        res.sendStatus(405);
-        console.log("/PUT no permitido");
-    });
-
-    // DELETE Recurso Completo
-    app.delete(API_PATH + "/libraries-stats", (req, res) => {
-
-        librariestats.remove({});
-
+    
+    
+    //DELETE /studentsAndalucia/
+    
+    app.delete(API_PATH + "/students-andalucia", (req, res) => {
+    
+        studentsAndalucia.remove({});
+    
         res.sendStatus(200);
+    
     });
-
-    // GET Recurso Concreto
-    app.get(API_PATH + "/libraries-stats/:city", (req, res) => {
+    
+    
+    //GET /studentsAndalucia/malaga/
+    
+    app.get(API_PATH + "/students-andalucia/:city", (req, res) => {
+    
         var city = req.params.city;
-
-        librariestats.find({ "city": city }).toArray((err, librariesList) => {
-            if (err) {
-                console.log("Error :" + err);
-            }
-            else if (librariesList.length >= 1) {
-                console.log("/GET a un Recurso Concreto");
-                res.send(librariesList.map((c) => {
-                    delete c._id;
-                    return c;
-                })[0]);
+    
+        studentsAndalucia.find({ "city": city}).toArray((err, studentsArray) => {
+            console.log(studentsArray.length);
+            if (err)
+                console.log("Error: " + err);
+    
+            if (studentsArray.length == 0) {
+                console.log("El recurso no se ha encontrado");
+                res.sendStatus(404);
             }
             else {
+                res.send(studentsArray.map((c) =>{
+                    delete c._id;
+                    return(c);
+                    
+                }));
+             console.log("Recurso encontrado y mostrado"); 
+            }
+        });
+    
+    });
+    
+    //GET /studentsAndalucia/malaga/2017
+    
+    app.get(API_PATH + "/students-andalucia/:city/:year", (req, res) => {
+    
+        var city = req.params.city;
+        var year = parseInt(req.params.year);
+    
+        studentsAndalucia.find({ "city": city, "year":year }).toArray((err, studentsArray) => {
+            if (err){
+                console.log("Error: " + err);
+            }
+            if(studentsArray.length >=1) {
+                console.log("GET a un recurso. Recurso encontrado");
+                res.send(studentsArray.map((c) =>{
+                    delete c._id;
+                    return(c);
+                })[0]);
+            }else {
+                console.log("GET a un recurso. El recurso no se ha encontrado");
                 res.sendStatus(404);
+            }
+        });
+    
+    });
+    
+    //PUT /studentsAndalucia/malaga/2017
+    
+    app.put(API_PATH + "/students-andalucia/:city/:year", (req, res) => {
+    
+        var city = req.params.city;
+        var year = parseInt(req.params.year);
+        var updateStudents = req.body;
+    
+        studentsAndalucia.find({ "city": city,"year": year }).toArray((err, studentsArray) => {
+            if (err){
+                console.log(err);
+            }
+            if (studentsArray.length == 0) {
+                console.log("PUT recurso no encontrado 404");
+                res.sendStatus(404);
+    
+            }else if (!updateStudents.city || !updateStudents.year ||!updateStudents.eso || !updateStudents.high || !updateStudents.vocational 
+                ||updateStudents.city != city || updateStudents.year != year|| Object.keys(updateStudents).length != 5 ){
+                    console.log("PUT recurso encontrado. Se intenta actualizar con campos no validos 400");
+                    res.sendStatus(400);
+        
+            }else {
+                    studentsAndalucia.updateOne({ "city": city}, { $set: updateStudents });
+                    studentsAndalucia.updateOne({ "year": year }, { $set: updateStudents });
+                    console.log("PUT realizado con exito");
+                    res.sendStatus(200);
+        
+            
+            }
+        });
+    
+    });
+    
+    //DELETE /studentsAndalucia/malaga/2017
+    
+    app.delete(API_PATH + "/students-andalucia/:city/:year", (req, res) => {
+    
+        var city = req.params.city;
+        var year = parseInt(req.params.year);
+    
+        studentsAndalucia.find({ "city": city, "year":year }).toArray((err, studentsArray) => {
+            if (err){
+                console.log(err);
+            }
+            if (studentsArray.length >=1) {
+                studentsAndalucia.remove({ "city": city, "year":year });
+                res.sendStatus(200);
+                console.log("DELETE a un recurso. Recurso borrado");
+            }else{
+                res.sendStatus(404);
+                console.log("DELETE a un recurso. Recurso no encontrado");
             }
         });
     });
     
-    //GET busqueda por 2 parametros
-
-    app.get(API_PATH + "/libraries-stats/:city/:year", (req, res) => {
-
-        var city = req.params.city;
-        var year = req.params.year;
-
-        librariestats.find({ "city": city, "year":year }).toArray((err, librariesArray) => {
-            if (err)
-                console.log("Error: " + err);
-
-            if (librariesArray == 0) {
-                res.sendStatus(404);
-            }
-            else {
-                res.send(librariesArray.map((c) =>{
-                    delete c._id;
-                    return(c);
-                
-                }));
-            }
-        });
-    });
-
-
-
-    //POST INCORRECTO
-    app.post(API_PATH + "/libraries-stats/:city", (req, res) => {
+    //POST incorrecto
+    app.post(API_PATH + "/students-andalucia/:city/:year", (req, res) => {
         res.sendStatus(405);
-        console.log("/POST no permitido");
     });
-
-    // PUT Recurso Concreto
-    app.put(API_PATH + "/libraries-stats/:city", (req, res) => {
-        var city = req.params.city;
-        var librarie = req.body;
-
-        if (city != librarie.city || Object.keys(librarie).length !== 5) {
-            res.sendStatus(400);
-            console.log(Date() + " - Hacking attemp!");
-        }
-        else {
-            librariestats.find({ "city": city }).toArray((err, librariesPut) => {
-                if (err) {
-                    console.log("Error :" + err);
-                }
-                else if (librariesPut.length == 0) {
-                    console.log("No hemos encontrado el elemento para actualizar");
-                    res.sendStatus(404);
-                }
-                else {
-                    librariestats.update({ "city": city }, librarie, (err, numUpdated) => {
-                        if (err) {
-                            console.log("Error " + err);
-                        }
-                        else {
-                            console.log(" - updated" + numUpdated);
-                            res.sendStatus(200);
-                        }
-                    });
-                }
-            });
-        }
+    
+    //PUT incorrecto
+    app.put(API_PATH + "/students-andalucia/", (req, res) => {
+        res.sendStatus(405);
     });
-    // DELETE Recurso Concreto
-    app.delete(API_PATH + "/libraries-stats/:city", (req, res) => {
-
-        var city = req.params.city;
-
-        librariestats.find({ "city": city }).toArray((err, librariesArray) => {
-            if (err)
-                console.log(err);
-
-            if (librariesArray == 0) {
-
-                res.sendStatus(404);
-
-            }
-            else {
-
-                librariestats.deleteOne({ "city": city });
-                res.sendStatus(200);
-            }
-        });
+    
+    app.get("/api/v1/students-andalucia/docs/", (req, res) => {
+        res.redirect("https://documenter.getpostman.com/view/6870023/S17qS957");
     });
-};
+    
+    /*var buscador = function(city, year, eso, high, vocational);
+    app.get(API_PATH + "/students-andalucia" , function(req, res) {
+       var city = req.params.city;
+       
+       if(){
+           
+       } 
+    });*/
+        
+    
+    }
