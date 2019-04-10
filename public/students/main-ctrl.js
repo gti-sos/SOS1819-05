8e9af42588d3d779ca4e8e2ccc05df6dc684d8dc
sos1819mvm <marta_v_m6@hotmail.com>
2019-04-10 09:41:04 +0000

    html marta

--- public/students/main-ctrl.js
+++ public/students/main-ctrl.js
@@ -1,76 +1,64 @@
+/* global angular */
+
 var app = angular.module("MiniPostmanApp");
 
 app.controller("MainCtrl", ["$scope","$http", function($scope, $http){
     console.log("Modular MainCtrl initialized!");
-    $scope.url = "https://sos1819-05.herokuapp.com/api/v1/students-andalucia";
-                
-    $scope.send = function(){
-        $http.get($scope.url).then(function(response){
-            $scope.data = JSON.stringify(response.data,null,2); 
-            $scope.estado=response.status;
-        });
-    };
-    
-    //POST
-    $scope.city = null;
-    $scope.year = null;
-    $scope.eso = null;
-    $scope.high = null;
-    $scope.vocational= null;
-    
-    //post textarea
-    $scope.postdata = function(city, year, eso, high, vocational) {
-        $http.post($scope.url, JSON.parse($scope.data)).then(function(response) {
-            $scope.estado = response.status;
-		});
-      };
-    
-    
-    //POST
-    $scope.postdataform = function(city,year,eso,high,vocational){
-        var dato = {
-            "city": city,
-            "year": year,
-            "eso": eso,
-            "high": high,
-            "vocational": vocational
-        };  
-        $http.post($scope.url, JSON.stringify(dato)).then(function(response) {
-            $scope.estado = response.status;
-		});
-    };
-    
-    //PUT
-    $scope.putdataform = function(city, year, eso, high, vocational) {
-         
-        $http.put($scope.url, JSON.parse($scope.data)).then(function(response) {
-            $scope.estado = response.status;
-        });
-    };
-      
-    //DELETE
-    $scope.deletedata = function(city, year, eso, high, vocational) {
-        var dato = {
-            "city": city,
-            "year": year,
-            "eso": eso,
-            "high": high,
-            "vocational": vocational
-        };
+    $scope.url = "https://sos1819-05.herokuapp.com/api/v1/students-andalucia/";
         
-        $http.delete($scope.url, JSON.stringify(dato)).then(function(response) {
-            $scope.estado = response.status;
+    //LoadInitialData
+    $scope.loadInitialData = function (){
+        $http.get($scope.url + "loadInitialData").then(function (response){
+            $scope.data = JSON.stringify(response.data,null,2);
+            $scope.statusInfo = JSON.stringify(response.status, null, 2);
+        }).catch(function (response) {
+		   	$scope.statusInfo = JSON.stringify(response.status, null, 2);
+		});
+    };
+         
+    //GET       
+    $scope.get = function(){
+        $http.get($scope.url).then(function(response){
+            $scope.status = response.status;
+            $scope.data = JSON.stringify(response.data,null,2);
+        }, function (error){
+            $scope.status = error.status;
+            $scope.data = "";
+        });
+    };
+
+    //POST
+    $scope.post = function(){
+        $http.post($scope.url,$scope.data).then(function(response){
+            $scope.status = response.status;
+            $scope.data = "";
+        }, function (error){
+            $scope.status = error.status;
+            $scope.data = "";
         });
     };
       
-    //loadInitialData
-    $scope.loadData = function() {
-          
-        $http.get($scope.url, JSON.stringify($scope.data)).then(function(response) {
-            $scope.estado = response.status;
+    //PUT            
+    $scope.put = function(){
+        $http.put($scope.url,$scope.data).then(function(response){
+            $scope.status = response.status;
+            $scope.data = "";
+        }, function (error){
+            $scope.status = error.status;
+            $scope.data = "";
         });
     };
-    
+         
+    //DELETE        
+    $scope.delete = function(){
+        $http.delete($scope.url).then(function(response){
+            $scope.status = response.status;
+            $scope.data = "";
+        }, function (error){
+            $scope.status = error.status;
+            $scope.data = "";
+        });
+    };
     
     
 }]);
