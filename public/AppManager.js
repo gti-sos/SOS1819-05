6f5fa25a7098aae4e9958ab5116c9a53898ca4b8
sos1819egv <enrique4597@gmail.com>
2019-04-24 09:03:51 +0000

    .

--- public/AppManager.js
+++ public/AppManager.js
@@ -9,12 +9,13 @@ angular
             
             /*ÁLVARO*/
             .when("/ui/v1/athletes-performance-sport", {
+             
                 templateUrl: "../athletesApi/index.html",
                 controller: "mainCtrl"
             })
             .when("/ui/v1/athletes-performance-sport/:city/:year", {
                 templateUrl: "../athletesApi/edit.html",
-                controller: "editCtrl"
+               controller: "editCtrl"
             });
 
             /*Enrique*/
