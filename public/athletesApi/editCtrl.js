fd50217cf1893e2ea5b4305491c1e37600e66b91
sos1819aom <alvortmed@alum.us.es>
2019-04-24 11:43:12 +0000

    v2

--- public/athletesApi/editCtrl.js
+++ public/athletesApi/editCtrl.js
@@ -5,10 +5,16 @@ app.controller("editCtrl", ["$scope", "$http", "$routeParams", "$location", func
     console.log("editCtrl inicializado");
     var API = "/api/v1/athletes-performance-sport" + "/" + $routeParams.city + "/" + $routeParams.year;
 
-    $http.get(API).then(function(res) {
-        console.log("Dato recibido del servidor: " + JSON.stringify(res.data, null, 2));
-        $scope.updatedAthlete = res.data;
-    });
+    refresh();
+
+    function refresh() {
+        console.log("Requesting athletes to <" + API + ">");
+        $http.get(API).then(function(res) {
+            console.log("Datos recibidos del servidor: " + JSON.stringify(res.data, null, 2));
+            $scope.updatedAthlete = res.data; //ya lo tenemos en el modelo. Ya podemos recogerlo desde la vista
+        });
+    }
+
 
     $scope.put = function() {
         console.log("Updating an athlete");
