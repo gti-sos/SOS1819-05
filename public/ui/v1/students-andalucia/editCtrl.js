9f61e07957729a4b3f83f24ac6e075b890bebabb
sos1819mvm <marta_v_m6@hotmail.com>
2019-05-14 16:05:37 +0000

    put

--- public/ui/v1/students-andalucia/editCtrl.js
+++ public/ui/v1/students-andalucia/editCtrl.js
@@ -12,26 +12,27 @@ angular
         console.log("Requesting athletes to <" + API + ">");
         $http.get(API).then(function(res) {
             console.log("Datos recibidos del servidor: " + JSON.stringify(res.data, null, 2));
-            $scope.updatedAthlete = res.data;
+            $scope.updatedStudent = res.data;
         });
     }
 
 
-        $scope.update = function() {
-            $http.put(API, $scope.updatedStudent).then(function(response) {
-                $scope.status = "Status: " + response.status;
-                alert("OK: estadistica actualizada");
-            }).catch(function(response) {
-                if (response.status == 405) {
-                    alert("Método no permitido");
-                }
-                if (response.status == 400) {
-                    alert("Asegurese de poner bien los datos");
-                }
-                $scope.estado = response.status;
+    $scope.update = function() {
+        $http.put(API, $scope.updatedStudent).then(function(response) {
+            $scope.status = "Status: " + response.status;
+            alert("OK: estadistica actualizada");
+        }).catch(function(response) {
+            if (response.status == 405) {
+                alert("Método no permitido");
+            }
+            if (response.status == 400) {
+                alert("Asegurese de poner bien los datos");
+            }
+            $scope.estado = response.status;
 
-            });
-            $location.path("StudentsApp"); //para volver
-            refresh();
-        };
+        });
+        $location.path("/"); //para volver al la vista principal
+        refresh();
+    };
+    
 }]);
